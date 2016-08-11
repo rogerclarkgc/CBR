a38db2046fbff257f373b898cde17e20864979af
Wolfson Liu <wolfson@users.noreply.github.com>
2016-08-11 19:56:38 +0800

    20160811 rearranged code

--- CBR.sql
+++ CBR.sql
@@ -1,4 +1,5 @@
 --this file is for the database for CBR
+
 CREATE TABLE recordhead(
                         rcode int PRIMARY KEY,--record code
                         lcode int, --locate code
@@ -48,45 +49,136 @@ CREATE TABLE location(
                      );
 
 
-COPY birdname FROM './Database/birdname.csv' WITH CSV;
+COPY birdname
+FROM './Database/birdname.csv'
+WITH CSV;
 
-COPY recordhead FROM './Database/rh1utf.csv' WITH CSV;
+COPY recordhead
+FROM './Database/rh1utf.csv'
+WITH CSV;
 
-COPY recordhead FROM './Database/rh2utf.csv' WITH CSV;
+COPY recordhead
+FROM './Database/rh2utf.csv'
+WITH CSV;
 
-COPY recordhead FROM './Database/rh3utf.csv' WITH CSV;
+COPY recordhead
+FROM './Database/rh3utf.csv'
+WITH CSV;
 
-COPY recordhead FROM './Database/rh4utf.csv' WITH CSV;
+COPY recordhead
+FROM './Database/rh4utf.csv'
+WITH CSV;
 
-COPY recordhead FROM './Database/rh5utf.csv' WITH CSV;
+COPY recordhead
+FROM './Database/rh5utf.csv'
+WITH CSV;
 
-COPY birdlist FROM './Database/birdlist1utf.csv' WITH CSV;
+COPY birdlist
+FROM './Database/birdlist1utf.csv'
+WITH CSV;
 
-COPY birdlist FROM './Database/birdlist2utf.csv' WITH CSV;
+COPY birdlist
+FROM './Database/birdlist2utf.csv'
+WITH CSV;
 
-COPY birdlist FROM './Database/birdlist3utf.csv' WITH CSV;
+COPY birdlist
+FROM './Database/birdlist3utf.csv'
+WITH CSV;
 
-COPY birdlist FROM './Database/birdlist4utf.csv' WITH CSV;
+COPY birdlist
+FROM './Database/birdlist4utf.csv'
+WITH CSV;
 
-COPY birdlist FROM './Database/birdlist5utf.csv' WITH CSV;
+COPY birdlist
+FROM './Database/birdlist5utf.csv'
+WITH CSV;
 
-INSERT INTO location (SELECT DISTINCT recordhead.lcode, recordhead.city, recordhead.spot FROM recordhead);  
+INSERT INTO location
+    (
+    SELECT DISTINCT recordhead.lcode, recordhead.city, recordhead.spot
+    FROM recordhead
+    );  
 
-SELECT count(*) FROM recordhead; --find the total number of records
+SELECT count(*)
+FROM recordhead; --find the total number of records
 
-SELECT count(*) FROM birdlist; --find the total number of item
+SELECT count(*)
+FROM birdlist; --find the total number of item
 
-COPY (SELECT DISTINCT lcode, city, spot FROM recordhead ORDER BY city) TO './locate.csv' WITH CSV;
+COPY
+    (
+    SELECT
+        DISTINCT lcode, city, spot
+    FROM recordhead
+    ORDER BY city
+    )
+TO './locate.csv'
+WITH CSV;
 
 --choose the spots with most records
 
-COPY (SELECT recordhead.city, spot, count(rcode) FROM recordhead GROUP BY city, spot ORDER BY count(rcode) DESC LIMIT 100) TO './topspot.csv' WITH CSV;  
+COPY
+    (
+    SELECT recordhead.city, spot, count(rcode)
+    FROM recordhead
+    GROUP BY city, spot
+    ORDER BY count(rcode) DESC LIMIT 100
+    )
+TO './topspot.csv'
+WITH CSV;  
 
 --choose the birds with most items
-COPY (SELECT birdlist.bcode, birdname.bname, count(birdlist.bcode) FROM birdlist, birdname WHERE birdlist.bcode=birdname.bcode GROUP BY birdlist.bcode, birdname.bname ORDER BY count(birdlist.bcode) DESC LIMIT 100) TO './topbird.csv';
+COPY
+    (
+    SELECT birdlist.bcode, birdname.bname, count(birdlist.bcode)
+    FROM birdlist, birdname
+    WHERE birdlist.bcode=birdname.bcode
+    GROUP BY birdlist.bcode, birdname.bname
+    ORDER BY count(birdlist.bcode) DESC
+    LIMIT 100
+    )
+TO './topbird.csv';
 
-COPY (SELECT birdlist.bcode, birdname.bname, avg(birdlist.amount) FROM birdlist, birdname WHERE birdlist.bcode=birdname.bcode GROUP BY birdlist.bcode, birdname.bname ORDER BY count(birdlist.bcode) DESC LIMIT 100) TO './topbirdper.csv';
+COPY
+    (
+    SELECT birdlist.bcode, birdname.bname, avg(birdlist.amount)
+    FROM birdlist, birdname
+    WHERE birdlist.bcode=birdname.bcode
+    GROUP BY birdlist.bcode, birdname.bname
+    ORDER BY count(birdlist.bcode) DESC
+    LIMIT 100
+    )
+TO './topbirdper.csv';
 
-COPY (SELECT DISTINCT birdlist.bcode, birdname.bname, birdname.family, recordhead.city, recordhead.spot FROM birdname, recordhead, birdlist WHERE birdlist.bcode=birdname.bcode AND birdlist.rcode=recordhead.rcode AND recordhead.spot='弄岗保护区' ORDER BY birdlist.bcode) TO './弄岗保护区.csv' WITH CSV;
+COPY
+    (
+    SELECT DISTINCT birdlist.bcode,
+           birdname.bname,
+           birdname.family,
+           recordhead.city,
+           recordhead.spot
+    FROM birdname, recordhead, birdlist
+    WHERE birdlist.bcode=birdname.bcode
+          AND birdlist.rcode=recordhead.rcode
+          AND recordhead.spot='弄岗保护区'
+    ORDER BY birdlist.bcode
+    )
+TO './弄岗保护区.csv'
+WITH CSV;
 
-COPY (SELECT DISTINCT birdlist.bcode, birdname.bname, birdname.family, recordhead.city, recordhead.spot FROM birdname, recordhead, birdlist WHERE birdlist.bcode=birdname.bcode AND birdlist.rcode=recordhead.rcode AND recordhead.spot='弄岗保护区陇瑞站' ORDER BY birdlist.bcode) TO './弄岗保护区陇瑞站.csv' WITH CSV;
+COPY
+    (
+    SELECT DISTINCT birdlist.bcode,
+           birdname.bname,
+           birdname.family,
+           recordhead.city,
+           recordhead.spot
+    FROM birdname, recordhead, birdlist
+    WHERE birdlist.bcode=birdname.bcode
+          AND birdlist.rcode=recordhead.rcode
+          AND recordhead.spot='弄岗保护区陇瑞站'
+    ORDER BY birdlist.bcode
+    )
+TO './弄岗保护区陇瑞站.csv'
+WITH CSV;
+
--- README.md
+++ README.md
@@ -1,11 +1,21 @@
-#Readme file of CBR
-1. CBRdownload.sh
-CBRdownload.sh is used to get the bird data from http://birdtalker.net/report/index.asp
+# CBRdownload#
 
-Data fetched from the web using CBRdownload.sh exits some errors, which demands 
-some modifications.
+## Description ##
 
-2. CBR.sql
-CBR.sql is used to set up the database to store the data.
+CBRdownload is a bash script used to download data from the [China Bird Report Center](http://birdtalker.net/report/index.asp) (中国观鸟记录中心).
 
-Data files can be found in dir:Database.
+[China Bird Report Center](http://birdtalker.net/report/index.asp) is a website for Chinese birders to upload their birding records. The web have changed for sometimes, so the CBRdownload might not work.
+
+The downloaded data forms several tables. And the tables can be stored in database, such as PostgreSQL. The relative SQL to make the database is stored in *CBR.sql*.
+
+The data already downloaded is in directory *Database*, which can be inputed into database directly.
+
+## Usage ##
+
+Run the CBRdownload.sh and input the information asked.
+
+## Information ##
+
+Author:  Wolfson
+License: LGPL-3
+Version: 0.1
